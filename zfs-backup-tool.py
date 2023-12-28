950ed4f8d203de693f37c7d8a29710f47fbc34db
Florian Sauer <sauer.florian@gmx.de>
2023-12-28 15:19:13 +0100

    list now skips uninitialized targets

--- zfs-backup-tool.py
+++ zfs-backup-tool.py
@@ -444,7 +444,7 @@ class ZfsBackupTool(object):
             existing_snapshot_paths: Set[str] = set()
             if not self.shell_command.file_exists(os.path.join(target, TARGET_SUBDIRECTORY, INITIALIZED_FILE_NAME),
                                                   self.config.remote):
-                print("Target {} is not initialized, skipping...".format(target), sys.stderr)
+                print("Target {} is not initialized, skipping...".format(target), file=sys.stderr)
                 continue
             self._get_file_paths(target, os.path.join(target, TARGET_SUBDIRECTORY), BACKUP_FILE_POSTFIX,
                                  existing_snapshot_paths)
