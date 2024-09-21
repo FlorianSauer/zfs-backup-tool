7988a1c1afbc364ad23c1344dcd13af4bad43674
Florian Sauer <sauer.florian@gmx.de>
2024-09-21 18:28:54 +0200

    fixing ssh host representation

--- ZfsBackupTool/ShellCommand/SshHost.py
+++ ZfsBackupTool/ShellCommand/SshHost.py
@@ -2,9 +2,12 @@ class SshHost(object):
     def __init__(self, host: str, user: str = None, port: int = None, key_path: str = None):
         self.host = host
         self.user = user
-        self.port = port
+        self.port = port or 22
         self.key_path = key_path
 
     def __str__(self):
+        return "{}@{}:{}".format(self.user, self.host, self.port)
+
+    def __repr__(self):
         keyfile_postfix = "" if self.key_path is None else " (key: {})".format(self.key_path)
         return "SshHost({}@{}:{}{})".format(self.user, self.host, self.port, keyfile_postfix)
